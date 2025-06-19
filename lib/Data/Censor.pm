f9a2301286587d99de58c4502bc817bf734a0758
David Precious <davidp@preshweb.co.uk>
2025-06-19 09:40:06 +0100

    Bump version to 0.04, update Changes

--- lib/Data/Censor.pm
+++ lib/Data/Censor.pm
@@ -13,11 +13,11 @@ Data::Censor - censor sensitive stuff in a data structure
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
